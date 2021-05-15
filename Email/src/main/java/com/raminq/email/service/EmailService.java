58be8d430bd47324d9f92edc354c73ca367b34f5
raminqdev <ramin.qamari93@gmail.com>
2021-05-15 22:32:13 +0430

    .

--- Email/src/main/java/com/raminq/email/service/EmailService.java
+++ Email/src/main/java/com/raminq/email/service/EmailService.java
@@ -2,11 +2,9 @@ package com.raminq.email.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
