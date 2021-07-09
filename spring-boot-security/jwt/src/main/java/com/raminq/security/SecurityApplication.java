40863fdaa2e43b2017e78898b188a23d51e02d7a
raminqdev <ramin.qamari93@gmail.com>
2021-07-09 09:57:28 +0430

    .

--- spring-boot-security/jwt/src/main/java/com/raminq/security/SecurityApplication.java
+++ spring-boot-security/jwt/src/main/java/com/raminq/security/SecurityApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SecurityApplication {
     public static void main(String[] args) {
         //System.setProperty("spring.main.lazy-initialization", "true");
+
         SpringApplication.run(SecurityApplication.class, args);
     }
 
